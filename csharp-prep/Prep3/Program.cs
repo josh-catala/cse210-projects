0ece7a51bcfcf12acd036633a8cba51243d977b9
josh.catala <jls.catala@gmail.com>
2026-01-21 15:37:34 -0700

    done + stretch challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "";
+
+        do
+        {   
+            int magic_num = RandomNumberGenerator.GetInt32(1,10);
+            Console.WriteLine("A random number from 1-10 has been generated.");
+            bool guessed = false;
+            int guess_count = 0;
+
+            while (guessed == false)
+                {
+                    Console.Write("What is your guess? ");
+                    string guess_input = Console.ReadLine();
+                    int guess_num = int.Parse(guess_input);
+                    guess_count++;
+
+                    if (guess_num == magic_num)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"Your guess count was: {guess_count}");
+                        guessed = true;
+                        Console.WriteLine("Would you like to play again (y/n)? ");
+                        play = Console.ReadLine();
+                    } 
+                    else if (guess_num > magic_num)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else if (guess_num < magic_num)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                }
+        } while (play == "y");
     }
 }
